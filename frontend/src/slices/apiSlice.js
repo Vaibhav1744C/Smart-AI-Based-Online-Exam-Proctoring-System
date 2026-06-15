1557170c9bf848d6b665b370a1cac4652a23491c
JASHoswal1234 <jashoswal269@gmail.com>
2026-06-15 15:02:27 +0530

    Fixed some bugs

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,13 +1,12 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+const baseQuery = fetchBaseQuery({
+  baseUrl: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000',
+  credentials: 'include',
+});
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_BACKEND_URL,
-    credentials: 'include', // This sends cookies automatically
-  }),
-
+  baseQuery,
   tagTypes: ['User'],
   // it like a prent to other api
   // it a build in builder
